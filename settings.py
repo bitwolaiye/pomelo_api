d04def28145908ec6af6f9190de877800af9adc1
ZhouQi <bitwolaiye@gmail.com>
2016-01-08 10:40:52 +0800

    for deploy

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'zhouqi'
 
-app_port = 8000
+app_port = 8100
 
 db_name = 'pomelo'
 db_user = ''
@@ -9,7 +9,7 @@ db_port = 5432
 db_password = ''
 db_host = '127.0.0.1'
 
-url_pre = '/item'
+url_pre = '/pomelo'
 
 notification_key_path = 'key.pem'
 notification_cert_path = 'cert.pem'
