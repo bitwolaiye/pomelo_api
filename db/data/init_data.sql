5ed23458a4818dd5316959cc38365723a84cd4f1
ZhouQi <bitwolaiye@gmail.com>
2016-01-29 16:19:00 +0800

    channel avatar

--- db/data/init_data.sql
+++ db/data/init_data.sql
@@ -5,7 +5,7 @@ INSERT INTO users(user_id, user_name, user_gender) VALUES (9, '柚小秘', 0);
 
 SELECT setval('users_user_id_seq', 10000);
 
-INSERT INTO channels(channel_id, channel_name, channel_avatar, channel_user_id) VALUES (1, '今天是个大晴天', NULL ,1);
-INSERT INTO channels(channel_id, channel_name, channel_avatar, channel_user_id) VALUES (2, '我就爱自拍', NULL ,2);
+INSERT INTO channels(channel_id, channel_name, channel_avatar, channel_user_id) VALUES (1, '今天是个大晴天', 'pomelo_1' ,1);
+INSERT INTO channels(channel_id, channel_name, channel_avatar, channel_user_id) VALUES (2, '我就爱自拍', 'pomelo_2' ,2);
 
 SELECT setval('channels_channel_id_seq', 10000);
